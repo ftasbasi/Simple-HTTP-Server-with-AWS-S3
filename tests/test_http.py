b204724f8fab71e249445aa28a966fa228fd2c85
furkan tasbasi <tasbasi01@gmail.com>
2021-08-07 16:53:42 +0300

    test5

--- tests/test_http.py
+++ tests/test_http.py
@@ -10,12 +10,3 @@ def test_request_response():
 
     # Confirm that the request-response cycle completed successfully.
     assert_true(response.ok)
-
-def test_request_response2():
-    url = 'http://localhost:{port}/picus/list/alican'.format(port="5000")
-
-    # Send a request to the mock API server and store the response.
-    response = requests.get(url)
-
-    # Confirm that the request-response cycle completed successfully.
-    assert_true(response.ok)
